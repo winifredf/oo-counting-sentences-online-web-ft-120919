daae409372d5845403c553dab1e7dc412e3c9c73
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 01:38:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,14 +11,23 @@ class String
   end
 
   def question?
-
+    if  self.end_with?("?")
+      return  true
+    else
+      return  false
+    end
   end
 
   def exclamation?
-
+    if  self.end_with?("!")
+      return  true
+    else
+      return  false
+    end
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject  {|string| string.empty?}
+    return  new_array.length
   end
 end
